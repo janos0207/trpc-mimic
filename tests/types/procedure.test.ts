c91ee36f1b7ff41c621e996fc6b7abe8e8f0ed70
janos0207 <janos0207terminal@gmail.com>
2025-08-14 01:29:51 +0900

    test: add Vitest setup

--- tests/types/procedure.test.ts
+++ tests/types/procedure.test.ts
@@ -1,10 +1,10 @@
 import { z, ZodVoid } from "zod";
-import { expectTypeOf } from "vitest";
+import { expectTypeOf, test } from "vitest";
 
 import { ProcedureDef } from "../../src/router";
 import { procedure } from "../../src/procedure";
 
-function test_infer_type_of_resolve() {
+test("test_infer_type_of_resolve", () => {
   const helloInput = z.object({ name: z.string() });
 
   const hello: ProcedureDef<typeof helloInput, string> = procedure()
@@ -13,14 +13,14 @@ function test_infer_type_of_resolve() {
 
   expectTypeOf(hello.resolve).parameter(0).toEqualTypeOf<{ name: string }>();
   expectTypeOf(hello.resolve).returns.toEqualTypeOf<string>();
-}
+});
 
-function test_infer_type_of_resolve_without_input() {
+test("test_infer_type_of_resolve_without_input", () => {
   const hello: ProcedureDef<ZodVoid, string> = procedure().query(() => "pong");
   expectTypeOf(hello.resolve).parameters.toEqualTypeOf<[]>();
-}
+});
 
-function test_infer_type_of_input() {
+test("test_infer_type_of_input", () => {
   const helloInput = z.object({ name: z.string() });
 
   const hello: ProcedureDef<typeof helloInput, string> = procedure()
@@ -29,8 +29,4 @@ function test_infer_type_of_input() {
   type InputType = z.infer<(typeof hello)["input"]>;
 
   expectTypeOf(hello.resolve).parameter(0).toEqualTypeOf<InputType>();
-}
-
-test_infer_type_of_resolve();
-test_infer_type_of_resolve_without_input();
-test_infer_type_of_input();
+});
---
+++ tests/types/router.test.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { createRouter } from "../../src/router";
+import { procedure } from "../../src/procedure";
+
+import { test } from "vitest";
+
+test("tentative", () => {
+  const router = createRouter()
+    .procedure(
+      "ping",
+      procedure().mutation(() => "pong")
+    )
+    .procedure(
+      "hello",
+      procedure()
+        .input(z.object({ name: z.string() }))
+        .query((input) => `Hello, ${input.name}!`)
+    )
+    .procedure(
+      "add",
+      procedure()
+        .input(z.object({ x: z.number(), y: z.number() }))
+        .mutation((input) => input.x + input.y)
+    );
+
+  router.call("ping");
+  router.call("hello", { name: "Yasu" });
+  // router.call("hello", { input: { x: 1 } });
+  router.call("add", { x: 1, y: 2 });
+  // router.call("unknown", {});  // Type Error
+
+  const a = procedure()
+    .input(z.object({ name: z.string() }))
+    .query((input) => `Hello, ${input.name}!`);
+});
